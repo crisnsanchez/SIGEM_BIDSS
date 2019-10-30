b88d96bf7fb2583fe357c351de010905e0372003
bayron997 <44510932+bayron997@users.noreply.github.com>
2019-10-30 10:10:08 -0600

    Implementation Login V1
    Se implemento el Login con las credenciales de Office365, (Reparar diseño)

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -25,5 +25,6 @@ namespace SIGEM_BIDSS.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
     }
 }
