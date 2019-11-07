d56422a07ba2b2820f902376150f3eef70d39420
pdiaz12 <paula.diaz@bi-dss.com>
2019-11-07 15:30:02 -0600

    Menú

---
+++ SIGEM_BIDSS/Controllers/MenuController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SIGEM_BIDSS.Controllers
+{
+    public class MenuController : Controller
+    {
+        // GET: Menu
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Catalogo()
+        {
+            return View();
+        }
+        public ActionResult Empleados()
+        {
+            return View();
+        }
+        public ActionResult Solicitud()
+        {
+            return View();
+        }
+    }
+}
