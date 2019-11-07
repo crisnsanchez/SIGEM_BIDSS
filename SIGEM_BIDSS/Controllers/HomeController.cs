359ab1b2f4b6a351138f8c62d049acdd454bed49
crisnsanchez <cristian.sanchez@bi-dss.com>
2019-11-07 02:34:34 -0600

    Login

--- SIGEM_BIDSS/Controllers/HomeController.cs
+++ SIGEM_BIDSS/Controllers/HomeController.cs
@@ -1,11 +1,4 @@
-﻿using Microsoft.Owin.Security;
-using Microsoft.Owin.Security.Cookies;
-using Microsoft.Owin.Security.OpenIdConnect;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace SIGEM_BIDSS.Controllers
 {
@@ -30,29 +23,5 @@ namespace SIGEM_BIDSS.Controllers
             return View();
         }
 
-
-        /// <summary>
-        /// Send an OpenID Connect sign-in request.
-        /// Alternatively, you can just decorate the SignIn method with the [Authorize] attribute
-        /// </summary>
-        public void SignIn()
-        {
-            if (!Request.IsAuthenticated)
-            {
-                HttpContext.GetOwinContext().Authentication.Challenge(
-                    new AuthenticationProperties { RedirectUri = "/" },
-                    OpenIdConnectAuthenticationDefaults.AuthenticationType);
-            }
-        }
-
-        /// <summary>
-        /// Send an OpenID Connect sign-out request.
-        /// </summary>
-        public void SignOut()
-        {
-            HttpContext.GetOwinContext().Authentication.SignOut(
-                    OpenIdConnectAuthenticationDefaults.AuthenticationType,
-                    CookieAuthenticationDefaults.AuthenticationType);
-        }
     }
 }
