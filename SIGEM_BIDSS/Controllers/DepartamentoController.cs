6ce1ed4b6c5fecd3949feec9646bef47e62cf20e
JJVallejo03 <44510925+JJVallejo03@users.noreply.github.com>
2019-11-13 21:47:07 -0600

    Revert "Cambios Division Politica"
    This reverts commit 13d32e4fc02884239e9615e7f44c18d63e8a2d28.

--- SIGEM_BIDSS/Controllers/DepartamentoController.cs
+++ SIGEM_BIDSS/Controllers/DepartamentoController.cs
@@ -206,7 +206,7 @@ namespace SIGEM_BIDSS.Controllers
 
             if (list != null)
             {
-                var itemToRemove = list.Single(r => r.mun_codigo == Municipios.mun_codigo);
+                var itemToRemove = list.Single(r => r.mun_UsuarioCrea == Municipios.mun_UsuarioCrea);
                 list.Remove(itemToRemove);
                 Session["tbMunicipio"] = list;
             }
