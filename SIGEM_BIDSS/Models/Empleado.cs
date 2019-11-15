bc7885438356747d7d0bc0541936b8437cac7665
pdiaz12 <paula.diaz@bi-dss.com>
2019-11-14 23:40:11 -0600

    Empleados

--- SIGEM_BIDSS/Models/Empleado.cs
+++ SIGEM_BIDSS/Models/Empleado.cs
@@ -21,56 +21,78 @@ namespace SIGEM_BIDSS.Models
 
     public class EmpleadosMetaData
     {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido")]
         [Display(Name = "ID EMPLEADO")]
         public short emp_Id { get; set; }
 
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido")]
         [Display(Name = "NOMBRES")]
         public string emp_Nombres { get; set; }
 
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido")]
         [Display(Name = "APELLIDOS")]
         public string emp_Apellidos { get; set; }
 
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido")]
         [Display(Name = "SEXO")]
         public string emp_Sexo { get; set; }
 
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido")]
         [Display(Name = "FECHA NACIMIENTO")]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy H:mm:ss tt}", ApplyFormatInEditMode = true)]
         public string emp_FechaNacimiento { get; set; }
 
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido")]
         [Display(Name = "IDENTIFICACIÓN")]
         public string emp_Identificacion { get; set; }
 
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido")]
         [Display(Name = "TELEFÓNO")]
         public string emp_Telefono { get; set; }
 
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido")]
         [Display(Name = "CORREO ELECTRONICO")]
         public string emp_CorreoElectronico { get; set; }
 
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido")]
         [Display(Name = "TIPO SANGRE")]
         public int tps_Id { get; set; }
 
-        [Display(Name = "PUESTO")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido")]
+        [Display(Name = "PUESTO DE TRABAJO")]
         public int pto_Id { get; set; }
 
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido")]
         [Display(Name = "FECHA INGRESÓ")]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy H:mm:ss tt}", ApplyFormatInEditMode = true)]
         public string emp_FechaIngreso { get; set; }
 
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido")]
         [Display(Name = "DIRECCIÓN")]
         public string emp_Direccion { get; set; }
+
+
 
         [Display(Name = "FECHA INACTIVACIÓN")]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy H:mm:ss tt}", ApplyFormatInEditMode = true)]
         public string emp_RazonInactivacion { get; set; }
 
+
+
         [Display(Name = "ESTADO")]
         public int est_Id { get; set; }
 
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido")]
         [Display(Name = "IMAGEN")]
         public string emp_PathImage { get; set; }
 
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido")]
         [Display(Name = "MUNICIPIO")]
         public string mun_Id { get; set; }
+
 
         [Display(Name = "USUARIO CREA")]
         public Nullable<int> emp_UsuarioCrea { get; set; }
