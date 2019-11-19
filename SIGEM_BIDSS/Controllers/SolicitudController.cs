9d5a458276c27ad9f90353aa1f826b7968baf989
pdiaz12 <paula.diaz@bi-dss.com>
2019-11-19 16:56:24 -0600

    :-1:

--- SIGEM_BIDSS/Controllers/SolicitudController.cs
+++ SIGEM_BIDSS/Controllers/SolicitudController.cs
@@ -40,10 +40,60 @@ namespace SIGEM_BIDSS.Controllers
             return PartialView(meter_id);
         }
 
+        ///BIND
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+ 
+        public ActionResult Create([Bind(Include = "emp_Id,tipsol_Id,pto_Id,tpsal_id,tmo_id,are_Id,tipmo_id,tpv_Id,sol_GralDescripcion,sol_GralJefeInmediato,sol_Acper_Anterior,sol_Acper_Nuevo,sol_UsuarioCrea,sol_FechaCrea,sol_UsuarioModifica,sol_FechaModifica,sol_AnsolMonto,pto_Id,tpsal_id,tpv_Id,sol_AnviHospedaje,sol_AnviTrasladoHacia,sol_Anvi_Cliente,sol_Anvi_LugarDestino,sol_Anvi_PropositoVisita,sol_AprCiudad,sol_AprNombreEmpresa,sol_AprRtn,sol_ApreCargoTecn,sol_ApreLink,sol_ApreCorreoAdm,sol_ApreContactoAdm,sol_ApreCorreoTecn,sol_ApreNombreTecn,sol_ApreTelefono,sol_ApreTelefonoTecn,sol_GralComentario,sol_GralJustificacion,sol_ReemNoFactura,sol_ReemProveedor,sol_ReemCargoA,sol_AprDireccion")] tbSolicitud tbSolicitud)
+        {
+            ViewBag.emp_Id = new SelectList(db.tbEmpleado, "emp_Id", "emp_Nombres");
+            ViewBag.tipsol_Id = new SelectList(db.tbTipoSolicitud, "tipsol_Id", "tipsol_Descripcion");
+            ViewBag.pto_Id = new SelectList(db.tbPuesto, "pto_Id", "pto_Descripcion");
+            ViewBag.tpsal_id = new SelectList(db.tbTipoSalario, "tpsal_id", "tpsal_Descripcion");
+            ViewBag.tmo_id = new SelectList(db.tbTipoMoneda, "tmo_id", "tmo_Abreviatura");
+            ViewBag.tipmo_id = new SelectList(db.tbTipoMovimiento, "tipmo_id", "tipmo_Movimiento");
+            ViewBag.tpv_Id = new SelectList(db.tbTipoViatico, "tpv_Id", "tpv_Descripcion");
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var MensajeError = "";
+                    IEnumerable<object> list = null;
+                    var _date = Convert.ToDateTime(tbSolicitud.sol_GralFechaSolicitud).ToShortDateString();
+                    list = db.UDP_Gral_CrearAccionPersonal(tbSolicitud.emp_Id, tbSolicitud.tipsol_Id, tbSolicitud.pto_Id, tbSolicitud.tpsal_id,
+                        tbSolicitud.tmo_id, tbSolicitud.are_Id, tbSolicitud.tipmo_id, tbSolicitud.tpv_Id, tbSolicitud.sol_GralDescripcion,
+                        tbSolicitud.sol_GralJefeInmediato, tbSolicitud.sol_GralCorreoJefeInmediato, tbSolicitud.sol_GralComentario, 
+                        tbSolicitud.sol_GralJustificacion, tbSolicitud.sol_GralFechaSolicitud, tbSolicitud.sol_Acper_Anterior,
+                        tbSolicitud.sol_Acper_Nuevo, _functions.GetUser(), _functions.DatetimeNow(), _functions.GetUser(),
+                        _functions.DatetimeNow());
+                    foreach (UDP_Gral_CrearAccionPersonal_Result AccionPersonal in list)
+                        MensajeError = AccionPersonal.MensajeError;
+                    if (MensajeError.StartsWith("-1"))
+                    {
+                    
+                        ModelState.AddModelError("", "No se pudo insertar el registro, favor contacte al administrador.");
+                        return View(tbSolicitud);
+                    }
+                    else
+                    {
+                        return RedirectToAction("AccionPersonal");
+                    }
+                }
+                catch (Exception Ex)
+                {
+       
+                    ModelState.AddModelError("", "No se pudo insertar el registro, favor contacte al administrador.");
+                    return View(tbSolicitud);
+                }
+            }
+            return View(tbSolicitud);
+        }
+
+ 
         [HttpPost]
         public ActionResult AccionPersonal(tbSolicitud AccionPersonal)
         {
-            //public ActionResult UpdateFacturaDetalle(tbFacturaDetalle EditFacturaDetalle)
+
             try
             {
                 tbSolicitud vAccionPersonal = db.tbSolicitud.Find(AccionPersonal.sol_Id);
@@ -53,7 +103,7 @@ namespace SIGEM_BIDSS.Controllers
                     AccionPersonal.tmo_id, AccionPersonal.are_Id, AccionPersonal.tipmo_id, AccionPersonal.tpv_Id, AccionPersonal.sol_GralDescripcion,
                     AccionPersonal.sol_GralJefeInmediato, AccionPersonal.sol_GralCorreoJefeInmediato, AccionPersonal.sol_GralComentario,
                     AccionPersonal.sol_GralJustificacion, AccionPersonal.sol_GralFechaSolicitud, AccionPersonal.sol_Acper_Anterior, AccionPersonal.sol_Acper_Nuevo,
-                    _functions.sol_UsuarioCrea(), _functions.DatetimeNow(), _functions.sol_UsuarioCrea(), _functions.DatetimeNow());
+                    _functions.GetUser(), _functions.DatetimeNow(), _functions.GetUser(), _functions.DatetimeNow());
 
                 foreach (UDP_Gral_CrearAccionPersonal_Result AccionPersonals in list)
                     MensajeError = AccionPersonals.MensajeError;
@@ -74,8 +124,8 @@ namespace SIGEM_BIDSS.Controllers
 
 
                 Ex.Message.ToString();
-                ModelState.AddModelError("", "No se pudo actualizar el registro, favor contacte al administrador.");
-                return RedirectToAction("Edit", "Factura");
+                ModelState.AddModelError("", "No se pudo crear el registro, favor contacte al administrador.");
+                return RedirectToAction("AccionPersonal", "Solicitud");
             }
         }
     }
--- SIGEM_BIDSS/Models/GeneralFunctions.cs
+++ SIGEM_BIDSS/Models/GeneralFunctions.cs
@@ -23,30 +23,32 @@ namespace SIGEM_BIDSS.Models
             DateTime dt = DateTimeOffset.UtcNow.ToOffset(TimeSpan.FromHours(-6)).DateTime;
             return dt;
         }
-        public int sol_UsuarioCrea()
+        public int GetUser()
         {
             int _user = 1;
             return _user;
         }
-        //public int tipsol_Id(int _TipoSolicitud)
-        //{
-        //    int tipsol = db.tbTipoSolicitud.Find(_TipoSolicitud).tipsol_Id;
-        //    return tipsol;
-        //}
-        //public object TipodeSolicitud()
-        //{
-        //    object obtipsol = new { AccionPersonal = TipodeSolicitudSearch("Accion Personal"),
-        //                            AnticipodeViaticos = TipodeSolicitudSearch("Anticipo de Viaticos")
-        //    };
-        //    return obtipsol;
-        //}
+        public int tipsol_Id(int _TipoSolicitud)
+        {
+            int tipsol = db.tbTipoSolicitud.Find(_TipoSolicitud).tipsol_Id;
+            return tipsol;
+        }
+        public object TipodeSolicitud()
+        {
+            object obtipsol = new
+            {
+                AccionPersonal = TipodeSolicitudSearch("Accion Personal"),
+                AnticipodeViaticos = TipodeSolicitudSearch("Anticipo de Viaticos")
+            };
+            return obtipsol;
+        }
 
-        //public int TipodeSolicitudSearch(string _strtiposolSearch)
-        //{
-        //    if (_strtiposolSearch == null || _strtiposolSearch == "") { return 0;}
-        //    int tipsol = (from _dbtts in db.tbTipoSolicitud where (_dbtts.tipsol_Descripcion == _strtiposolSearch) select (_dbtts.tipsol_Id)).FirstOrDefault();
-        //    return tipsol;
-        //}
+        public int TipodeSolicitudSearch(string _strtiposolSearch)
+        {
+            if (_strtiposolSearch == null || _strtiposolSearch == "") { return 0; }
+            int tipsol = (from _dbtts in db.tbTipoSolicitud where (_dbtts.tipsol_Descripcion == _strtiposolSearch) select (_dbtts.tipsol_Id)).FirstOrDefault();
+            return tipsol;
+        }
 
 
         //public const string AccionPersonal = "Accion Personal";
