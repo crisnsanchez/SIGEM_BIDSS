aba26c3a2b6cb40a629bd3176b9d291301bf595f
pdiaz12 <paula.diaz@bi-dss.com>
2019-11-24 04:35:08 -0600

    js para validaciones de solicitud

---
+++ SIGEM_BIDSS/Scripts/app/ValidacionesGenerales.js
@@ -0,0 +1,33 @@
+﻿//DATE PICKET
+$("#sol_PerFechaInicio,#sol_PerFechaRegreso,#sol_GralFechaSolicitud").datepicker({
+    dateFormat: 'mm/dd/yy',
+    monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+    dayNamesMin: ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sá'],
+    minDate: '-100Y',
+    maxDate: '-18Y',
+    prevText: 'Ant',
+    nextText: 'Sig',
+    changeMonth: true,
+    changeYear: true,
+    monthNamesShort: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic']
+}).datepicker('setDate', new Date());
+
+
+
+//////SOLO LETRAS
+
+function soloLetras(e) {
+    tecla = (document.all) ? e.keyCode : e.which;
+    tecla = String.fromCharCode(tecla)
+    return /^[a-zA-ZáéíóúñÁÉÍÓÚÑ ]+$/.test(tecla);
+}
+
+
+
+////SOLO NUMEROS 
+function soloNumeros(e) {
+
+    tecla = (document.all) ? e.keyCode : e.which;
+    tecla = String.fromCharCode(tecla)
+    return /^[0-9]+$/.test(tecla);
+}
