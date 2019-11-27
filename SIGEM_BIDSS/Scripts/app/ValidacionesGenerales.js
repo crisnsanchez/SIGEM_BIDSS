93b275e3be2e374eb65887b5a04a750e0d1f1c24
pdiaz12 <paula.diaz@bi-dss.com>
2019-11-26 23:14:13 -0600

    Anticipo Salario y Funcionalidad de Solicitud Permiso laboral, solicitud viatiicos
    Grupal

--- SIGEM_BIDSS/Scripts/app/ValidacionesGenerales.js
+++ SIGEM_BIDSS/Scripts/app/ValidacionesGenerales.js
@@ -1,10 +1,42 @@
 ﻿//----------Datepicker
-$('#sol_GralFechaSolicitud').datepicker({
+$('#sol_PerFechaInicio,#sol_PerFechaRegreso,#sol_GralFechaSolicitud,#sol_PerFechaMedioDia').datepicker({
     format: "dd/mm/yyyy",
     startDate: "01/01/1990",
     language: "es",
     daysOfWeekDisabled: "0"
 });
+
+
+
+
+
+
+
+
+
+//HI
+
+//    function ShowHideDiv(chkCantDias) {
+//                var sol_PerMedioDia = document.getElementById("sol_PerFechaMedioDia");
+//    sol_PerMedioDia.style.display = chkCantDias.checked ? "block" : "none";
+//}
+      
+//    <label for="chkCantDias">
+
+//        ¿Permiso Medio Dia?
+//            <input type="checkbox" id="chkCantDias" onclick="ShowHideDiv(this)" />
+//    </label>
+//    <hr />
+//    <div id="sol_PerFechaMedioDia" style="display: none">
+
+//        @Html.LabelFor(model => model.sol_PerFechaMedioDia, htmlAttributes: new { @class = "control-label col-md-2" })
+//            <div class="col-md-7">
+//            @Html.EditorFor(model => model.sol_PerFechaMedioDia, new {htmlAttributes = new { @class = "form-control" } })
+//                @Html.ValidationMessageFor(model => model.sol_PerFechaMedioDia, "", new { @class = "text-danger" })
+//            </div>
+//    </div>
+
+
 
 
 //////SOLO LETRAS
