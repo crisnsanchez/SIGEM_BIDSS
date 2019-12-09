0d6a33e58a18f7357227f123db2d86d90350a778
JJVallejo03 <44510925+JJVallejo03@users.noreply.github.com>
2019-12-09 15:30:40 -0600

    Scripts

--- SIGEM_BIDSS/Scripts/app/General_PCAE.js
+++ SIGEM_BIDSS/Scripts/app/General_PCAE.js
@@ -1,23 +1,6 @@
 ﻿//JS General para Pantallas Create y Edit
 
 
-document.getElementById('insf_Correo').addEventListener('input', function () {
-    campo = event.target;
-    valido = document.getElementById('emailEoK');
-    //selector = document.getElementById('emp_CorreoElectronico')
-    emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
-    //Se muestra un texto a modo de ejemplo, luego va a ser un icono
-    if (emailRegex.test(campo.value)) {
-        valido.innerText = "";
-        $('#insf_Correo').removeClass('is-invalid');
-    } else {
-        valido.innerText = "Correo Inválido";
-        $('#insf_Correo').focus();
-        $('#insf_Correo').addClass('is-invalid');
-    }
-});
-
-
 document.addEventListener("input", function () {
     $("input[type='text']", 'form').each(function () {
         _id = $(this).attr("id");
@@ -85,3 +68,20 @@ document.getElementById("Save").onclick = function () {
     txtObj.value = txtObj.value.replace(/\s+$/, ""); //Right trim
 };
 
+
+
+document.getElementById('insf_Correo').addEventListener('input', function () {
+    campo = event.target;
+    valido = document.getElementById('emailEoK');
+    //selector = document.getElementById('emp_CorreoElectronico')
+    emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+    //Se muestra un texto a modo de ejemplo, luego va a ser un icono
+    if (emailRegex.test(campo.value)) {
+        valido.innerText = "";
+        $('#insf_Correo').removeClass('is-invalid');
+    } else {
+        valido.innerText = "Correo Inválido";
+        $('#insf_Correo').focus();
+        $('#insf_Correo').addClass('is-invalid');
+    }
+});
