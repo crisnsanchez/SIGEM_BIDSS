750af0210ed8e3348132d2223eff286df73d6379
JJVallejo03 <44510925+JJVallejo03@users.noreply.github.com>
2019-12-21 15:36:13 -0600

    Liquidacion Detalle

---
+++ SIGEM_BIDSS/Controllers/LiquidacionAnticipoViaticoDetalleController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SIGEM_BIDSS.Models;
+
+namespace SIGEM_BIDSS.Controllers
+{
+    public class LiquidacionAnticipoViaticoDetalleController : Controller
+    {
+        private SIGEM_BIDSSEntities db = new SIGEM_BIDSSEntities();
+
+        // GET: LiquidacionAnticipoViaticoDetalle
+        public ActionResult Index()
+        {
+            var tbLiquidacionAnticipoViaticoDetalle = db.tbLiquidacionAnticipoViaticoDetalle.Include(t => t.tbLiquidacionAnticipoViatico).Include(t => t.tbTipoViatico);
+            return View(tbLiquidacionAnticipoViaticoDetalle.ToList());
+        }
+
+        // GET: LiquidacionAnticipoViaticoDetalle/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbLiquidacionAnticipoViaticoDetalle tbLiquidacionAnticipoViaticoDetalle = db.tbLiquidacionAnticipoViaticoDetalle.Find(id);
+            if (tbLiquidacionAnticipoViaticoDetalle == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbLiquidacionAnticipoViaticoDetalle);
+        }
+
+        // GET: LiquidacionAnticipoViaticoDetalle/Create
+        public ActionResult Create()
+        {
+            ViewBag.Lianvi_Id = new SelectList(db.tbLiquidacionAnticipoViatico, "Lianvi_Id", "Lianvi_Correlativo");
+            ViewBag.tpv_Id = new SelectList(db.tbTipoViatico, "tpv_Id", "tpv_Descripcion");
+            return View();
+        }
+
+        // POST: LiquidacionAnticipoViaticoDetalle/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Lianvide_Id,Lianvi_Id,Lianvide_FechaGasto,tpv_Id,Lianvide_MontoGasto,Lianvide_Concepto,Lianvide_Archivo,Lianvide_UsuarioCrea,Lianvide_FechaCrea,Lianvide_UsuarioModifica,Lianvide_FechaModifica")] tbLiquidacionAnticipoViaticoDetalle tbLiquidacionAnticipoViaticoDetalle)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tbLiquidacionAnticipoViaticoDetalle.Add(tbLiquidacionAnticipoViaticoDetalle);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Lianvi_Id = new SelectList(db.tbLiquidacionAnticipoViatico, "Lianvi_Id", "Lianvi_Correlativo", tbLiquidacionAnticipoViaticoDetalle.Lianvi_Id);
+            ViewBag.tpv_Id = new SelectList(db.tbTipoViatico, "tpv_Id", "tpv_Descripcion", tbLiquidacionAnticipoViaticoDetalle.tpv_Id);
+            return View(tbLiquidacionAnticipoViaticoDetalle);
+        }
+
+        // GET: LiquidacionAnticipoViaticoDetalle/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbLiquidacionAnticipoViaticoDetalle tbLiquidacionAnticipoViaticoDetalle = db.tbLiquidacionAnticipoViaticoDetalle.Find(id);
+            if (tbLiquidacionAnticipoViaticoDetalle == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Lianvi_Id = new SelectList(db.tbLiquidacionAnticipoViatico, "Lianvi_Id", "Lianvi_Correlativo", tbLiquidacionAnticipoViaticoDetalle.Lianvi_Id);
+            ViewBag.tpv_Id = new SelectList(db.tbTipoViatico, "tpv_Id", "tpv_Descripcion", tbLiquidacionAnticipoViaticoDetalle.tpv_Id);
+            return View(tbLiquidacionAnticipoViaticoDetalle);
+        }
+
+        // POST: LiquidacionAnticipoViaticoDetalle/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Lianvide_Id,Lianvi_Id,Lianvide_FechaGasto,tpv_Id,Lianvide_MontoGasto,Lianvide_Concepto,Lianvide_Archivo,Lianvide_UsuarioCrea,Lianvide_FechaCrea,Lianvide_UsuarioModifica,Lianvide_FechaModifica")] tbLiquidacionAnticipoViaticoDetalle tbLiquidacionAnticipoViaticoDetalle)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tbLiquidacionAnticipoViaticoDetalle).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Lianvi_Id = new SelectList(db.tbLiquidacionAnticipoViatico, "Lianvi_Id", "Lianvi_Correlativo", tbLiquidacionAnticipoViaticoDetalle.Lianvi_Id);
+            ViewBag.tpv_Id = new SelectList(db.tbTipoViatico, "tpv_Id", "tpv_Descripcion", tbLiquidacionAnticipoViaticoDetalle.tpv_Id);
+            return View(tbLiquidacionAnticipoViaticoDetalle);
+        }
+
+        // GET: LiquidacionAnticipoViaticoDetalle/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbLiquidacionAnticipoViaticoDetalle tbLiquidacionAnticipoViaticoDetalle = db.tbLiquidacionAnticipoViaticoDetalle.Find(id);
+            if (tbLiquidacionAnticipoViaticoDetalle == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbLiquidacionAnticipoViaticoDetalle);
+        }
+
+        // POST: LiquidacionAnticipoViaticoDetalle/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tbLiquidacionAnticipoViaticoDetalle tbLiquidacionAnticipoViaticoDetalle = db.tbLiquidacionAnticipoViaticoDetalle.Find(id);
+            db.tbLiquidacionAnticipoViaticoDetalle.Remove(tbLiquidacionAnticipoViaticoDetalle);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
