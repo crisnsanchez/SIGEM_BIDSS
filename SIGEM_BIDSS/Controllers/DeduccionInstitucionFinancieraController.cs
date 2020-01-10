3f993674d8462d0e9c930b9c5cbd8099de96c9d1
JJVallejo03 <44510925+JJVallejo03@users.noreply.github.com>
2020-01-10 11:04:33 -0600

    Deduccion Financiera

---
+++ SIGEM_BIDSS/Controllers/DeduccionInstitucionFinancieraController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SIGEM_BIDSS.Models;
+
+namespace SIGEM_BIDSS.Controllers
+{
+    public class DeduccionInstitucionFinancieraController : BaseController
+    {
+        private SIGEM_BIDSSEntities db = new SIGEM_BIDSSEntities();
+
+        // GET: DeduccionInstitucionFinanciera
+        public ActionResult Index()
+        {
+            var tbDeduccionInstitucionFinanciera = db.tbDeduccionInstitucionFinanciera.Include(t => t.tbEmpleado).Include(t => t.tbInstitucionFinanciera);
+            return View(tbDeduccionInstitucionFinanciera.ToList());
+        }
+
+        // GET: DeduccionInstitucionFinanciera/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbDeduccionInstitucionFinanciera tbDeduccionInstitucionFinanciera = db.tbDeduccionInstitucionFinanciera.Find(id);
+            if (tbDeduccionInstitucionFinanciera == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbDeduccionInstitucionFinanciera);
+        }
+
+        // GET: DeduccionInstitucionFinanciera/Create
+        public ActionResult Create()
+        {
+            ViewBag.emp_Id = new SelectList(db.tbEmpleado, "emp_Id", "emp_Nombres");
+            ViewBag.insf_IdInstitucionFinanciera = new SelectList(db.tbInstitucionFinanciera, "insf_Id", "insf_Nombre");
+            return View();
+        }
+
+        // POST: DeduccionInstitucionFinanciera/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "deif_IdDeduccionInstFinanciera,emp_Id,insf_IdInstitucionFinanciera,deif_Monto,deif_Comentarios,deif_UsuarioCrea,deif_FechaCrea,deif_UsuarioModifica,deif_FechaModifica,deif_Activo")] tbDeduccionInstitucionFinanciera tbDeduccionInstitucionFinanciera)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tbDeduccionInstitucionFinanciera.Add(tbDeduccionInstitucionFinanciera);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.emp_Id = new SelectList(db.tbEmpleado, "emp_Id", "emp_Nombres", tbDeduccionInstitucionFinanciera.emp_Id);
+            ViewBag.insf_IdInstitucionFinanciera = new SelectList(db.tbInstitucionFinanciera, "insf_Id", "insf_Nombre", tbDeduccionInstitucionFinanciera.insf_IdInstitucionFinanciera);
+            return View(tbDeduccionInstitucionFinanciera);
+        }
+
+        // GET: DeduccionInstitucionFinanciera/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbDeduccionInstitucionFinanciera tbDeduccionInstitucionFinanciera = db.tbDeduccionInstitucionFinanciera.Find(id);
+            if (tbDeduccionInstitucionFinanciera == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.emp_Id = new SelectList(db.tbEmpleado, "emp_Id", "emp_Nombres", tbDeduccionInstitucionFinanciera.emp_Id);
+            ViewBag.insf_IdInstitucionFinanciera = new SelectList(db.tbInstitucionFinanciera, "insf_Id", "insf_Nombre", tbDeduccionInstitucionFinanciera.insf_IdInstitucionFinanciera);
+            return View(tbDeduccionInstitucionFinanciera);
+        }
+
+        // POST: DeduccionInstitucionFinanciera/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "deif_IdDeduccionInstFinanciera,emp_Id,insf_IdInstitucionFinanciera,deif_Monto,deif_Comentarios,deif_UsuarioCrea,deif_FechaCrea,deif_UsuarioModifica,deif_FechaModifica,deif_Activo")] tbDeduccionInstitucionFinanciera tbDeduccionInstitucionFinanciera)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tbDeduccionInstitucionFinanciera).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.emp_Id = new SelectList(db.tbEmpleado, "emp_Id", "emp_Nombres", tbDeduccionInstitucionFinanciera.emp_Id);
+            ViewBag.insf_IdInstitucionFinanciera = new SelectList(db.tbInstitucionFinanciera, "insf_Id", "insf_Nombre", tbDeduccionInstitucionFinanciera.insf_IdInstitucionFinanciera);
+            return View(tbDeduccionInstitucionFinanciera);
+        }
+
+        // GET: DeduccionInstitucionFinanciera/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbDeduccionInstitucionFinanciera tbDeduccionInstitucionFinanciera = db.tbDeduccionInstitucionFinanciera.Find(id);
+            if (tbDeduccionInstitucionFinanciera == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbDeduccionInstitucionFinanciera);
+        }
+
+        // POST: DeduccionInstitucionFinanciera/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tbDeduccionInstitucionFinanciera tbDeduccionInstitucionFinanciera = db.tbDeduccionInstitucionFinanciera.Find(id);
+            db.tbDeduccionInstitucionFinanciera.Remove(tbDeduccionInstitucionFinanciera);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
